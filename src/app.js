78bf2a95acfef87c8e6fa4e7813d21086e2575ee
jackgeckoboard <jack@geckoboard.com>
2016-06-01 11:34:13 +0100

    Added router, styles and public and repos page

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
+import Router from './router'
+import styles from './styles/main.styl'
+
 window.app = {
   init () {
-    
+    this.router = new Router()
+    this.router.history.start()
   }
 }
 
